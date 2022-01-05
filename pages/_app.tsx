02788305f259c9347b2c818823b8761735b337b8
Luiz <lh.alvesf@gmail.com>
2022-01-05 16:15:01 -0300

    ~

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap");
+  }, []);
   return <Component {...pageProps} />
 }
 
