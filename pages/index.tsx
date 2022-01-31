34b949be6f04a74db0adc3d272e766c8db1fdb01
Luiz <lh.alvesf@gmail.com>
2022-01-30 22:02:51 -0300

    teste

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
-import type { NextPage } from 'next'
+import React, { useEffect, useCallback, useState } from 'react'
+import { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import axios from 'axios'
+
 const Home: NextPage = () => {
+
+    const [portfolios, setPortfolios] = useState<Array<any>>([]);
+    /* functions */
+    const fetchPortfolio = useCallback(() => {
+      axios
+        .get('https://atest666.herokuapp.com/api/posts?populate=*')
+        .then(response => {
+
+          const data = response.data.data.map((data: any) => {return data.attributes;});
+          setPortfolios(data);
+          console.log(portfolios);
+        })
+        .catch(error => { console.log(error);});
+    }, [portfolios]);
+
+    /* effects */
+    useEffect(() => { fetchPortfolio(); }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,57 +33,157 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <section className='container px-5 py-5 mt-5'>
+        <div className='d-flex'>
 
-      <div className='container mt-5 d-flex align-content-center justify-content-center'>
-        <div className='mt-5 d-flex flex-column justify-content-center'>
-          <h2 className='text-center mb-5'>Site em construção</h2>
-          <iframe className="d-block float-center giphy-embed" src="https://giphy.com/embed/ule4vhcY1xEKQ" width="480" height="480" frameBorder="0" allowFullScreen></iframe>
+          <div className='pe-5'>
+            <Image src="/image.jpg" alt="" className='rounded-circle' width={200} height={200} />
+          </div>
+
+          <div className='pt-3'>
+            <h2 className='title'>Luiz Ferreira</h2>
+            <div className={styles.lineDecorator}></div>
+            <ul className='list-inline'>
+              <li className='list-inline-item'>
+                <Link href={'/work'}>
+                  WORK
+                </Link>
+              </li>
+              <li className='list-inline-item'>
+                <Link href={'/projects'}>
+                  PROJECTS
+                </Link>
+              </li>
+              <li className='list-inline-item'>
+                <Link href={'/articles'}>
+                  ARTICLES
+                </Link>
+              </li>
+              <li className='list-inline-item'>
+                <Link href={'/contact'}>
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
+
+            <br />
+            <br />
+            <br />
+
+            <section>
+              <h3>Hi, I'm Luiz</h3>
+              <p className='lead'>
+                I'm a web developer based in São Paulo, BR. I’m currently SR frontend developer at a local agency, Maac Tech.
+                In my spare time I like working with a few select clients and building my own products on Kurupako
+                <br />
+                My most recent product is Kurutaco. It’s a simple, but powerful, calorie tracker app.
+                <br />
+                {/* You can usually find me on Twitter, but I'm occasionally on Product Hunt and Reddit as well. */}
+              </p>
+            </section>
+
+
+          </div>
+
         </div>
-      </div>
+
+      </section>
 
 
-      {/* navigation */}
-      {/* <header className="d-flex flex-wrap justify-content-center mb-4 border-bottom">
-        <nav className="w-100 navbar navbar-dark bg-dark" aria-label="First navbar example">
-          <Link href="/">
-            <a className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-              <i className="bi bi-code"  ></i>
-              <span className="fs-4">Luiz Ferreira</span>
-            </a>
-          </Link>
-          <ul className="nav nav-pills">
-            <li className="nav-item">
-              <Link href="/">
-                <a className="nav-link text-white">portfolio</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/">
-                <a className="nav-link text-white">about</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/">
-                <a className="nav-link text-white">blog</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/">
-                <a className="nav-link text-white">contact</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header> */}
+      {/* <section>
+        <h2>Hi, I'm Luiz</h2>
+        <p>I'm a .</p>
 
-      {/* <footer className='w-100 d-flex justify-content-center text-center px-3'>
+
+        <div className='d-flex'>
+          <div
+            className='d-flex shadow rounded align-items-end justify-content-center pb-3'
+            style={{
+              width: 350,
+              height: 250,
+            }}
+          >
+            <h3>Project Name</h3>
+          </div>
+        </div>
+      </section>
+ */}
+
+{/*
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <section>
+        <h2>Projects</h2>
+        <p>Here you can see some projects I've contributed.</p>
+
+
+        <div className='d-flex'>
+          <div
+            className='d-flex shadow rounded align-items-end justify-content-center pb-3'
+            style={{
+              width: 350,
+              height: 250,
+            }}
+          >
+            <h3>Project Name</h3>
+          </div>
+        </div>
+      </section>
+
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <section>
+        <h2>Experience</h2>
+        <p>
+          I've worked with a lot of programing langs and my prefered are: typescript and python. Nowadays I spend most of my time write projects using tools like AdonisJs, ReactJs, ReactNative and so on..
+        </p>
+        <div>
+          <h3>timeline</h3>
+
+        </div>
+      </section> */}
+
+      <footer className='w-100 d-flex justify-content-center text-center px-3 fixed-bottom'>
         <p>
           Luiz Ferreira &copy; { new Date().getFullYear() } &nbsp; | &nbsp;
           <i className="bi bi-github"></i>&nbsp;&nbsp;
           <i className="bi bi-telegram"></i>&nbsp;&nbsp;
           <i className="bi bi-linkedin"></i>
         </p>
-      </footer> */}
+      </footer>
 
     </div>
   )
---
+++ pages/projects/index.tsx
@@ -0,0 +1,82 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+
+const Projects: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Luiz Ferreira | software developer based in Brazil. | Javascript, Typescript, Python</title>
+        <meta name="description" content="Luiz Ferreira | software developer based in Brazil. | Javascript, Typescript, Python" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <section className='container px-5 py-5 mt-5'>
+        <div className='d-flex'>
+
+          <div className='pe-5'>
+            <Image src="/image.jpg" alt="" className='rounded-circle' width={200} height={200} />
+          </div>
+
+          <div className='pt-3'>
+            <h2 className='title'>Luiz Ferreira</h2>
+            <div className={styles.lineDecorator}></div>
+            <ul className='list-inline'>
+              <li className='list-inline-item'>
+                <Link href={'/work'}>
+                  WORK
+                </Link>
+              </li>
+              <li className='list-inline-item'>
+                <Link href={'/projects'}>
+                  PROJECTS
+                </Link>
+              </li>
+              <li className='list-inline-item'>
+                <Link href={'/articles'}>
+                  ARTICLES
+                </Link>
+              </li>
+              <li className='list-inline-item'>
+                <Link href={'/contact'}>
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
+
+            <br />
+            <br />
+            <br />
+
+            <section>
+              <h3>Projects</h3>
+              <p className='lead'>
+                There's no projects yet.
+                <br />
+                {/* You can usually find me on Twitter, but I'm occasionally on Product Hunt and Reddit as well. */}
+              </p>
+            </section>
+
+
+          </div>
+
+        </div>
+
+      </section>
+
+      <footer className='w-100 d-flex justify-content-center text-center px-3 fixed-bottom'>
+        <p>
+          Luiz Ferreira &copy; { new Date().getFullYear() } &nbsp; | &nbsp;
+          <i className="bi bi-github"></i>&nbsp;&nbsp;
+          <i className="bi bi-telegram"></i>&nbsp;&nbsp;
+          <i className="bi bi-linkedin"></i>
+        </p>
+      </footer>
+
+    </div>
+  )
+}
+
+export default Projects
